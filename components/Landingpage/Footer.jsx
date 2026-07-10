b9c8c0af9af78c28f76f274af93a08ec583bdbc3
manish kumar <manishkumarstudent8744@gmail.com>
2026-07-10 18:03:23 +0530

    logo chnage

--- components/Landingpage/Footer.jsx
+++ components/Landingpage/Footer.jsx
@@ -82,11 +82,11 @@ export default function Footer() {
             {/* LOGO */}
             <Link href="/" className="inline-block">
               <Image
-                src="/logo.png"
+                src="/logo4.png"
                 alt="Corechem Corporation"
-                width={330}
-                height={120}
-                className=" object-contain bg-white rounded-md p-2"
+                width={300}
+                height={20}
+                className=" object-cover h-25 w-80 bg-white rounded-md p-1"
               />
             </Link>
 
--- components/Landingpage/Navbar.jsx
+++ components/Landingpage/Navbar.jsx
@@ -82,7 +82,7 @@ export default function Navbar() {
           {/* LOGO */}
           <Link href="/" className="shrink-0">
             <Image
-              src="/logo.png"
+              src="/logo4.png"
               alt="Logo"
               width={260}
               height={100}
